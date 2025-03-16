1628eb1beb000ca27853ed6f793c5e1bdf4d023c
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2025-03-16 23:04:45 +0200

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test
+{
+    private static int x = 4;
+    
+	public static void main(String []args)
+	{
+		System.out.print("X value is: " + x + ".\n");
+		System.out.println("X value is: " + x + ".");
+		System.out.printf("X value is: %d.\n", x);
+	}
+}
